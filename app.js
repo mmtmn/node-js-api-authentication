3287d52777ecb46fc8f63b414f95b964202ba01d
Thiago M. Nóbrega <42742390+mmtmn@users.noreply.github.com>
2020-09-18 16:08:48 -0300

    Add auth token

--- app.js
+++ app.js
@@ -15,4 +15,19 @@ app.post('/api/posts', (req, res) => {
     });
 });
 
+app.post('/api/login', (req, res) => {
+    // Mock user
+    const user = {
+        id: 1,
+        username: 'mmtmn',
+        email: 'mmtmn@mmtmn.com'
+    }
+    
+    jwt.sign({user}, 'secretkey', (err, token) => {
+        res.json({
+            token
+        });
+    });
+});
+
 app.listen(5000, () => console.log('Local server running on port 5000'));
