aa5a6f04bb114820a35f41cce18f583eba51f0e4
Thiago M. Nóbrega <42742390+mmtmn@users.noreply.github.com>
2020-09-18 17:02:19 -0300

    Add route protection in posts

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-
 const app = express();
 
 app.get('/api', (req, res) => {
@@ -9,9 +8,16 @@ app.get('/api', (req, res) => {
     });
 });
 
-app.post('/api/posts', (req, res) => {
-    res.json({
-        message: 'Post created'
+app.post('/api/posts', verifyToken, (req, res) => {
+    jwt.verify(req.token, 'secretkey', (err, authData) => {
+        if(err) {
+            res.sendStatus(403);
+        } else {
+            res.json({
+                message: 'Post created',
+                authData
+            });
+        }
     });
 });
 
@@ -22,12 +28,35 @@ app.post('/api/login', (req, res) => {
         username: 'mmtmn',
         email: 'mmtmn@mmtmn.com'
     }
-    
-    jwt.sign({user}, 'secretkey', (err, token) => {
+
+    jwt.sign({user}, 'secretkey', { expiresIn: '30s'}, (err, token) => {
         res.json({
             token
         });
     });
 });
+
+// Format of token
+// Authorization: Bearer <acess_token>
+
+// Verify Token
+function verifyToken(req, res, next) {
+    // Get auth header value
+    const bearerHeader = req.headers['authorization'];
+    // Check if bearer is undefined
+    if(typeof bearerHeader !== 'undefined') {
+        // Split at the space
+        const bearer = bearerHeader.split(' ');
+        // Get token from array
+        const bearerToken = bearer[1];
+        // Set the token
+        req.token = bearerToken;
+        // Next middlerware
+        next();
+    } else {
+        // Forbidden
+        res.sendStatus(403);
+    }
+}
 
 app.listen(5000, () => console.log('Local server running on port 5000'));
